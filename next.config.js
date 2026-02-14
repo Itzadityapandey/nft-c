b454b489a759f9109fd209e3aff03afe4d72e503
Aditya Kumar <103852730+Itzadityapandey@users.noreply.github.com>
2026-02-14 16:03:13 +0530

    Add additional image domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['gateway.pinata.cloud'],
+    domains: [
+      'gateway.pinata.cloud', // For your NFTs
+      'api.dicebear.com',     // For the Agents
+      'upload.wikimedia.org'  // For the Background
+    ],
   },
 }
+
+module.exports = nextConfig
