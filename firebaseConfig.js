281ed0851c5a985de8cd8c8ffb6c41b98d7c0ab4
Aditya Kumar <103852730+Itzadityapandey@users.noreply.github.com>
2026-02-14 16:17:16 +0530

    Update Firebase configuration with project details

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_FROM_FIREBASE_CONSOLE",
-  authDomain: "PASTE_YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "PASTE_YOUR_DATABASE_URL",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyC0TGT1ddMFBp8sfkPnNFrSMbxjqahI6kI",
+  authDomain: "artoffice-af07c.firebaseapp.com",
+  databaseURL: "https://artoffice-af07c-default-rtdb.firebaseio.com",
+  projectId: "artoffice-af07c",
+  storageBucket: "artoffice-af07c.firebasestorage.app",
+  messagingSenderId: "177521622680",
+  appId: "1:177521622680:web:d23894cdf07c334d675aed",
+  measurementId: "G-PR0GQ5HMBC"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
+const analytics = getAnalytics(app);
